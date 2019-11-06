d5157472a73c145065935bbd0ca5117b0be315e1
Darrin Holst <darrinholst@gmail.com>
2019-11-05 18:37:20 -0600

    renaming

--- AddYnabTransaction/index.js
+++ AddYnabTransaction/index.js
@@ -1,32 +1,46 @@
-const {API} = require('ynab');
+const ynab = require('ynab');
+
+const MATCHERS = [
+    {
+        accountId: 'a90346df-e8d2-4b12-b534-3d90d15dcf5a',
+        regexp: /Account ending in (\d+).*on (\d{2}\/\d{2}\/\d{4}), at (.*), .*in the amount of (\S+)/s,
+        fields: {who: 1, when: 2, where: 3, amount: 4},
+    },
+    {
+        accountId: 'a4fa5bab-e050-4488-970c-c4429214f89b',
+        regexp: /Location ?: ?([^,]*).*Date ?: ?(\d{2}\/\d{2}\/\d{4}).*Amount ?: ?(\S+)/s,
+        fields: {who: 1, when: 2, where: 1, amount: 3},
+    },
+];
 
 module.exports = async function(context, req) {
-    if (!validateRequest(req.query)) return (context.res = {status: 400});
-
     try {
         const {budgetId} = req.query;
-        const transaction = buildTransaction(req.query);
-        await ynab().transactions.createTransaction(budgetId, {transaction});
-        context.res = {status: 204};
+        const {accountId, parts, fields} = findMatcher(req.body.toString());
+        const who = parts[fields.who];
+        const when = parts[fields.when];
+        const where = parts[fields.where];
+        const amount = parts[fields.amount];
+
+        const transaction = {
+            account_id: accountId,
+            date: new Date(when).toISOString().split('T')[0],
+            amount: -amount.replace(/\D/g, '') * 10,
+            payee_name: where,
+            memo: who,
+        };
+
+        const api = new ynab.API(process.env.YNAB_TOKEN);
+        await api.transactions.createTransaction(budgetId, {transaction});
+        context.res = {status: 200};
     } catch (e) {
-        context.res = {status: 500, body: JSON.stringify(e, null, 2)};
+        context.log(e);
+        context.res = {status: 400};
     }
 };
 
-function ynab() {
-    return new API(process.env.YNAB_TOKEN);
-}
-
-function validateRequest({budgetId, accountId, when, amount, where, who}) {
-    return who && when && where && amount && accountId && budgetId;
-}
-
-function buildTransaction({accountId, when, amount, where, who}) {
-    return {
-        account_id: accountId,
-        date: new Date(when).toISOString().split('T')[0],
-        amount: -amount.replace(/\D/g, '') * 10,
-        payee_name: where,
-        memo: who
-    };
+function findMatcher(body) {
+    const matcher = MATCHERS.find(({regexp}) => regexp.test(body));
+    if (!matcher) throw new Error(body);
+    return {...matcher, parts: matcher.regexp.exec(body)};
 }
