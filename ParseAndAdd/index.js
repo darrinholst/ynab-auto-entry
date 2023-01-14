c157e3d8c89a53e0bfec8156a517003834fe4aa2
Darrin Holst <darrinholst@gmail.com>
2023-01-13 18:40:27 -0600

    what kind of animal uses 4 spaces?

--- ParseAndAdd/index.js
+++ ParseAndAdd/index.js
@@ -1,102 +1,100 @@
 const ynab = require('ynab');
-const {DateTime} = require('luxon');
+const { DateTime } = require('luxon');
 
 const MATCHERS = [
-    {
-        accountId: 'a90346df-e8d2-4b12-b534-3d90d15dcf5a',
-        regexp: /ending in (\d+).*on (\d{1,2}\/\d{1,2}\/\d{4}), at (.*), .*in the amount of (\S+)/s,
-        fields: {who: 1, when: 2, where: 3, amount: 4},
-    },
-    {
-        accountId: 'a4fa5bab-e050-4488-970c-c4429214f89b',
-        regexp: /Location ?: ?([^,]*).*Date ?: ?(\d{1,2}\/\d{1,2}\/\d{4}).*Amount ?: ?(\S+)/s,
-        fields: {who: 1, when: 2, where: 1, amount: 3},
-    },
-    {
-        accountId: 'f4d3a509-068e-45bc-98c5-5bdc8d9cc40a',
-        regexp: /Pending charge for (\S+) on (\d{1,2}\/\d{1,2}).*at ([^,]*).*ending in (\d{1,4})\./is,
-        fields: {who: 4, when: 2, where: 3, amount: 1},
-    },
-    {
-        accountId: 'f4d3a509-068e-45bc-98c5-5bdc8d9cc40a',
-        regexp: /Pending charge on (\d{1,2}\/\d{1,2}).*at ([^,]*).*ending in (\d{1,4})\./is,
-        fields: {who: 3, when: 1, where: 2, amount: 99},
-    },
+  {
+    accountId: 'a90346df-e8d2-4b12-b534-3d90d15dcf5a',
+    regexp: /ending in (\d+).*on (\d{1,2}\/\d{1,2}\/\d{4}), at (.*), .*in the amount of (\S+)/s,
+    fields: { who: 1, when: 2, where: 3, amount: 4 },
+  },
+  {
+    accountId: 'a4fa5bab-e050-4488-970c-c4429214f89b',
+    regexp: /Location ?: ?([^,]*).*Date ?: ?(\d{1,2}\/\d{1,2}\/\d{4}).*Amount ?: ?(\S+)/s,
+    fields: { who: 1, when: 2, where: 1, amount: 3 },
+  },
+  {
+    accountId: 'f4d3a509-068e-45bc-98c5-5bdc8d9cc40a',
+    regexp: /Pending charge for (\S+) on (\d{1,2}\/\d{1,2}).*at ([^,]*).*ending in (\d{1,4})\./is,
+    fields: { who: 4, when: 2, where: 3, amount: 1 },
+  },
+  {
+    accountId: 'f4d3a509-068e-45bc-98c5-5bdc8d9cc40a',
+    regexp: /Pending charge on (\d{1,2}\/\d{1,2}).*at ([^,]*).*ending in (\d{1,4})\./is,
+    fields: { who: 3, when: 1, where: 2, amount: 99 },
+  },
 ];
 
 module.exports = async function (context, req) {
-    try {
-        context.log('Running...');
-        const {budgetId} = req.query;
-        const {accountId, parts, fields} = findMatcher(req.body.toString());
-        const who = parts[fields.who];
-        const when = normalizeTransactionDate(parts[fields.when]);
-        const where = parts[fields.where].replace(/=\n/, '');
-        const amount = parts[fields.amount] || '$1.00';
-        const transactionTime = DateTime.local()
-            .setZone('America/Chicago')
-            .toLocaleString(DateTime.TIME_24_SIMPLE);
+  try {
+    context.log('Running...');
+    const { budgetId } = req.query;
+    const { accountId, parts, fields } = findMatcher(req.body.toString());
+    const who = parts[fields.who];
+    const when = normalizeTransactionDate(parts[fields.when]);
+    const where = parts[fields.where].replace(/=\n/, '');
+    const amount = parts[fields.amount] || '$1.00';
+    const transactionTime = DateTime.local().setZone('America/Chicago').toLocaleString(DateTime.TIME_24_SIMPLE);
 
-        context.log(`Matched to ${accountId}`);
+    context.log(`Matched to ${accountId}`);
 
-        const {name, isApproved} = normalizePayee(where);
-        const transaction = {
-            account_id: accountId,
-            date: new Date(when).toISOString().split('T')[0],
-            amount: -amount.replace(/\D/g, '') * 10,
-            payee_name: name,
-            memo: `${transactionTime} ${who}`,
-            approved: isApproved,
-        };
+    const { name, isApproved } = normalizePayee(where);
+    const transaction = {
+      account_id: accountId,
+      date: new Date(when).toISOString().split('T')[0],
+      amount: -amount.replace(/\D/g, '') * 10,
+      payee_name: name,
+      memo: `${transactionTime} ${who}`,
+      approved: isApproved,
+    };
 
-        const api = new ynab.API(process.env.YNAB_TOKEN);
-        await api.transactions.createTransaction(budgetId, {transaction});
-        context.res = {status: 200};
-    } catch (e) {
-        context.log(e);
-        context.res = {status: 400};
-    }
+    const api = new ynab.API(process.env.YNAB_TOKEN);
+    await api.transactions.createTransaction(budgetId, { transaction });
+    context.res = { status: 200 };
+  } catch (e) {
+    context.log(e);
+    context.res = { status: 400 };
+  }
 };
 
 function normalizeTransactionDate(date) {
-    return date.length < 6 ? `${date}/${new Date().getFullYear()}` : date;
+  return date.length < 6 ? `${date}/${new Date().getFullYear()}` : date;
 }
 
 function normalizePayee(payee) {
-    const normalized = [
-        ['^amazon', 'Amazon'],
-        ['^amzn', 'Amazon'],
-        ['^apple.com', 'Apple'],
-        ['^casey', "Casey's"],
-        ['^dollar', 'Dollar Store'],
-        ['^fareway', 'Fareway', true],
-        ['^fleet', 'Fleet Farm'],
-        ['^harland', 'Ace Hardware'],
-        ['^hy-vee', 'Hy-Vee'],
-        ['^lowe', "Lowe's"],
-        ['^mcdonald', 'McDonalds'],
-        ['^menards', 'Menards'],
-        ['^orscheln', 'Orschelens'],
-        ['^petsmart', 'Petsmart'],
-        ['^sams', 'Sams Club'],
-        ['^target', 'Target'],
-        ['^wal-mart', 'Walmart'],
-        ['^wm super', 'Walmart'],
-        ['ampride', 'Ampride', true],
-        ['car wash', 'Car Wash'],
-        ['home depot', 'Home Depot'],
-        ['petco', 'Petco'],
-        ['xbox', 'Microsoft'],
-        ['chuys', 'Chuys'],
-        ['Store', 'McDonalds'],
-        ['.*', payee],
-    ].find(([regex]) => new RegExp(regex, 'i').test(payee));
+  const normalized = [
+    ['^amazon', 'Amazon'],
+    ['^amzn', 'Amazon'],
+    ['^apple.com', 'Apple'],
+    ['^casey', "Casey's"],
+    ['^dollar', 'Dollar Store'],
+    ['^fareway', 'Fareway', true],
+    ['^fleet', 'Fleet Farm'],
+    ['^harland', 'Ace Hardware'],
+    ['^hy-vee', 'Hy-Vee'],
+    ['^lowe', "Lowe's"],
+    ['^mcdonald', 'McDonalds'],
+    ['^menards', 'Menards'],
+    ['^orscheln', 'Orschelens'],
+    ['^petsmart', 'Petsmart'],
+    ['^sams', 'Sams Club'],
+    ['^target', 'Target'],
+    ['^wal-mart', 'Walmart'],
+    ['^wm super', 'Walmart'],
+    ['ampride', 'Ampride', true],
+    ['car wash', 'Car Wash'],
+    ['home depot', 'Home Depot'],
+    ['petco', 'Petco'],
+    ['xbox', 'Microsoft'],
+    ['chuys', 'Chuys'],
+    ['Store', 'McDonalds'],
+    ['.*', payee],
+  ].find(([regex]) => new RegExp(regex, 'i').test(payee));
 
-    return {name: normalized[1], isApproved: Boolean(normalized[2])};
+  return { name: normalized[1], isApproved: Boolean(normalized[2]) };
 }
 
 function findMatcher(body) {
-    const matcher = MATCHERS.find(({regexp}) => regexp.test(body));
-    if (!matcher) throw new Error(body);
-    return {...matcher, parts: matcher.regexp.exec(body)};
+  const matcher = MATCHERS.find(({ regexp }) => regexp.test(body));
+  if (!matcher) throw new Error(body);
+  return { ...matcher, parts: matcher.regexp.exec(body) };
 }
--- ParseAndAdd/index.spec.js
+++ ParseAndAdd/index.spec.js
@@ -8,154 +8,154 @@ const f = require('./index');
 
 chai.use(sinonChai);
 chai.use(dirtyChai);
-const {expect} = chai;
+const { expect } = chai;
 
 describe('ParseAndAdd', () => {
-    const ACCOUNT_1 = `
-      Card ending in 1234
-      on 8/5/1972, at Who's Lounge, in the amount of $452.99 fumullins
-    `;
+  const ACCOUNT_1 = `
+    Card ending in 1234
+    on 8/5/1972, at Who's Lounge, in the amount of $452.99 fumullins
+  `;
 
-    const ACCOUNT_2 = `
-      <p>A purchase exceeding the amount you specified has occurred</p>
-      <p>Location : Who's Lounge, Madrid, IA<BR>Transaction Date : 08/05/1972<BR>Purchase Amount : $452.99</p>
-    `;
+  const ACCOUNT_2 = `
+    <p>A purchase exceeding the amount you specified has occurred</p>
+    <p>Location : Who's Lounge, Madrid, IA<BR>Transaction Date : 08/05/1972<BR>Purchase Amount : $452.99</p>
+  `;
 
-    const ACCOUNT_3 = `
-      <p>Pending charge for $452.99 on 08/05 13:39 EDT at Who's =
+  const ACCOUNT_3 = `
+    <p>Pending charge for $452.99 on 08/05 13:39 EDT at Who's =
 Lounge, PERRY IA for Credit card ending in 1234.</p>
-    `;
+  `;
 
-    const ACCOUNT_3_NO_AMOUNT = `
-      <p>Pending charge on 08/05 20:26 EDT at CASEYS GEN STORE, WOODWARD IA for Credit card ending in 1234.</p>
-    `;
+  const ACCOUNT_3_NO_AMOUNT = `
+    <p>Pending charge on 08/05 20:26 EDT at CASEYS GEN STORE, WOODWARD IA for Credit card ending in 1234.</p>
+  `;
 
-    const BUILD_EXAMPLE = (payee) => `
-      Card ending in 1234
-      on 08/05/1972, at ${payee}, in the amount of $452.99 fumullins
-    `;
+  const BUILD_EXAMPLE = (payee) => `
+    Card ending in 1234
+    on 08/05/1972, at ${payee}, in the amount of $452.99 fumullins
+  `;
 
-    let context;
-    let request;
-    let ynabApi;
+  let context;
+  let request;
+  let ynabApi;
 
-    beforeEach(() => {
-        sinon.useFakeTimers(1586389116831);
-        ynabApi = {transactions: {createTransaction: sinon.stub()}};
-        sinon.stub(ynab, 'API').returns(ynabApi);
-        context = {log: sinon.stub()};
-        request = {query: {budgetId: 'budget-id'}};
-    });
+  beforeEach(() => {
+    sinon.useFakeTimers(1586389116831);
+    ynabApi = { transactions: { createTransaction: sinon.stub() } };
+    sinon.stub(ynab, 'API').returns(ynabApi);
+    context = { log: sinon.stub() };
+    request = { query: { budgetId: 'budget-id' } };
+  });
 
-    afterEach(() => {
-        sinon.restore();
-    });
+  afterEach(() => {
+    sinon.restore();
+  });
 
-    it('should add transaction for account 1', async () => {
-        request.body = ACCOUNT_1;
+  it('should add transaction for account 1', async () => {
+    request.body = ACCOUNT_1;
 
-        await f(context, request);
+    await f(context, request);
 
-        expect(context.res).to.eql({status: 200});
-        expect(ynabApi.transactions.createTransaction)
-            .calledOnce()
-            .calledWith('budget-id', {
-                transaction: {
-                    account_id: 'a90346df-e8d2-4b12-b534-3d90d15dcf5a',
-                    amount: -452990,
-                    date: '1972-08-05',
-                    memo: '18:38 1234',
-                    payee_name: "Who's Lounge",
-                    approved: false,
-                },
-            });
-    });
+    expect(context.res).to.eql({ status: 200 });
+    expect(ynabApi.transactions.createTransaction)
+      .calledOnce()
+      .calledWith('budget-id', {
+        transaction: {
+          account_id: 'a90346df-e8d2-4b12-b534-3d90d15dcf5a',
+          amount: -452990,
+          date: '1972-08-05',
+          memo: '18:38 1234',
+          payee_name: "Who's Lounge",
+          approved: false,
+        },
+      });
+  });
 
-    it('should add transaction for account 2', async () => {
-        request.body = ACCOUNT_2;
+  it('should add transaction for account 2', async () => {
+    request.body = ACCOUNT_2;
 
-        await f(context, request);
+    await f(context, request);
 
-        expect(context.res).to.eql({status: 200});
-        expect(ynabApi.transactions.createTransaction)
-            .calledOnce()
-            .calledWith('budget-id', {
-                transaction: {
-                    account_id: 'a4fa5bab-e050-4488-970c-c4429214f89b',
-                    amount: -452990,
-                    date: '1972-08-05',
-                    memo: "18:38 Who's Lounge",
-                    payee_name: "Who's Lounge",
-                    approved: false,
-                },
-            });
-    });
+    expect(context.res).to.eql({ status: 200 });
+    expect(ynabApi.transactions.createTransaction)
+      .calledOnce()
+      .calledWith('budget-id', {
+        transaction: {
+          account_id: 'a4fa5bab-e050-4488-970c-c4429214f89b',
+          amount: -452990,
+          date: '1972-08-05',
+          memo: "18:38 Who's Lounge",
+          payee_name: "Who's Lounge",
+          approved: false,
+        },
+      });
+  });
 
-    it('should add transaction for account 3', async () => {
-        request.body = ACCOUNT_3;
+  it('should add transaction for account 3', async () => {
+    request.body = ACCOUNT_3;
 
-        await f(context, request);
+    await f(context, request);
 
-        expect(context.res).to.eql({status: 200});
-        expect(ynabApi.transactions.createTransaction)
-            .calledOnce()
-            .calledWith('budget-id', {
-                transaction: {
-                    account_id: 'f4d3a509-068e-45bc-98c5-5bdc8d9cc40a',
-                    amount: -452990,
-                    date: '2020-08-05',
-                    memo: '18:38 1234',
-                    payee_name: "Who's Lounge",
-                    approved: false,
-                },
-            });
-    });
+    expect(context.res).to.eql({ status: 200 });
+    expect(ynabApi.transactions.createTransaction)
+      .calledOnce()
+      .calledWith('budget-id', {
+        transaction: {
+          account_id: 'f4d3a509-068e-45bc-98c5-5bdc8d9cc40a',
+          amount: -452990,
+          date: '2020-08-05',
+          memo: '18:38 1234',
+          payee_name: "Who's Lounge",
+          approved: false,
+        },
+      });
+  });
 
-    it('should add transaction for account 3 with no amount', async () => {
-        request.body = ACCOUNT_3_NO_AMOUNT;
+  it('should add transaction for account 3 with no amount', async () => {
+    request.body = ACCOUNT_3_NO_AMOUNT;
 
-        await f(context, request);
+    await f(context, request);
 
-        expect(context.res).to.eql({status: 200});
-        expect(ynabApi.transactions.createTransaction)
-            .calledOnce()
-            .calledWith('budget-id', {
-                transaction: {
-                    account_id: 'f4d3a509-068e-45bc-98c5-5bdc8d9cc40a',
-                    amount: -1000,
-                    date: '2020-08-05',
-                    memo: '18:38 1234',
-                    payee_name: "Casey's",
-                    approved: false,
-                },
-            });
-    });
+    expect(context.res).to.eql({ status: 200 });
+    expect(ynabApi.transactions.createTransaction)
+      .calledOnce()
+      .calledWith('budget-id', {
+        transaction: {
+          account_id: 'f4d3a509-068e-45bc-98c5-5bdc8d9cc40a',
+          amount: -1000,
+          date: '2020-08-05',
+          memo: '18:38 1234',
+          payee_name: "Casey's",
+          approved: false,
+        },
+      });
+  });
 
-    [
-        ['FAREWAY STORES', 'Fareway'],
-        ['DOLLAR-GENERAL', 'Dollar Store'],
-        ['HY-VEE #1234', 'Hy-Vee'],
-        ['Microsoft*Xbox', 'Microsoft'],
-        ["Casey's General", "Casey's"],
-        ['Store', 'McDonalds'],
-    ].forEach(([actual, expected]) => {
-        it(`should normalize the ${expected} payee`, async () => {
-            request.body = BUILD_EXAMPLE(actual);
+  [
+    ['FAREWAY STORES', 'Fareway'],
+    ['DOLLAR-GENERAL', 'Dollar Store'],
+    ['HY-VEE #1234', 'Hy-Vee'],
+    ['Microsoft*Xbox', 'Microsoft'],
+    ["Casey's General", "Casey's"],
+    ['Store', 'McDonalds'],
+  ].forEach(([actual, expected]) => {
+    it(`should normalize the ${expected} payee`, async () => {
+      request.body = BUILD_EXAMPLE(actual);
 
-            await f(context, request);
+      await f(context, request);
 
-            expect(ynabApi.transactions.createTransaction)
-                .calledOnce()
-                .calledWith('budget-id', {
-                    transaction: {
-                        account_id: 'a90346df-e8d2-4b12-b534-3d90d15dcf5a',
-                        amount: -452990,
-                        date: '1972-08-05',
-                        memo: '18:38 1234',
-                        payee_name: expected,
-                        approved: /fareway/i.test(expected),
-                    },
-                });
+      expect(ynabApi.transactions.createTransaction)
+        .calledOnce()
+        .calledWith('budget-id', {
+          transaction: {
+            account_id: 'a90346df-e8d2-4b12-b534-3d90d15dcf5a',
+            amount: -452990,
+            date: '1972-08-05',
+            memo: '18:38 1234',
+            payee_name: expected,
+            approved: /fareway/i.test(expected),
+          },
         });
     });
+  });
 });
